e810c9fa40d508882ce86944a97cf2b6de6efd3a
wijayaerick <wijaya.erick52@gmail.com>
2018-03-09 21:52:09 +0700

    write output and log

---
+++ Makefile
@@ -0,0 +1,9 @@
+all: src/bitonic_sort.c
+	gcc -g -Wall -o bitonic_sort src/bitonic_sort.c -fopenmp
+
+run:
+	./bitonic_sort 1024 3
+
+clean:
+	rm -rf bitonic_sort
+	rm -rf bitonic_sort.exe
--- src/bitonic_sort.c
+++ src/bitonic_sort.c
@@ -20,76 +20,133 @@
  http://www.iti.fh-flensburg.de/lang/algorithmen/sortieren/bitonic/bitonicen.htm 
  */
 
-/* 
+/*
 ------- ---------------------- 
-   Nikos Pitsianis, Duke CS 
+  Nikos Pitsianis, Duke CS 
 -----------------------------
+*/
+
+/* 
+  modified by: Erick Wijaya / 131515057
 */
 
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <time.h>
+#include <omp.h>
 
 struct timeval startwtime, endwtime;
 double seq_time;
+FILE *output_file;
+FILE *log_file;
 
-
-int N;          // data array size
-int *a;         // data array to be sorted
+int N;     // data array size
+int *arr;  // arr data array
+int num_thread;
+int test_amount;
 
 const int ASCENDING  = 1;
 const int DESCENDING = 0;
-
 
 void init(void);
 void print(void);
 void sort(void);
 void test(void);
-inline void exchange(int i, int j);
+static inline void exchange(int i, int j);
 void compare(int i, int j, int dir);
 void bitonicMerge(int lo, int cnt, int dir);
 void recBitonicSort(int lo, int cnt, int dir);
 void impBitonicSort(void);
+void parBitonicSort(void);
 
 
 /** the main program **/ 
 int main(int argc, char **argv) {
 
-  if (argc != 2) {
-    printf("Usage: %s n\n  where n is problem size (power of two)\n", argv[0]);
+  if (argc == 2) {
+    test_amount = 3;
+  } else if (argc == 3 && atoi(argv[2]) > 0) {
+    test_amount = atoi(argv[2]);
+  } else {
+    printf("Usage: %s n x\n  where n is problem size\n", argv[0]);
+    printf("  where x is test amount (optional with default=3, must be greater than 0)\n");
     exit(1);
   }
 
   N = atoi(argv[1]);
-  a = (int *) malloc(N * sizeof(int));
+  arr = (int *) malloc(N * sizeof(int));
+  num_thread = omp_get_max_threads();
+
+  log_file = fopen("output/log.txt", "a");
+  if (log_file == NULL) {
+    printf("Error: can't open/create file");
+    exit(1);
+  }
   
-  init();
+  time_t rawtime;
+  struct tm* timeinfo;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  printf("-----------------------------------------\n");
+  fprintf(log_file, "-----------------------------------------\n");
+  fprintf(log_file, "%s", asctime(timeinfo));
+  fprintf(log_file, "Problem Size: %d\n", N);
+  fprintf(log_file, "-----------------------------------------\n");
 
-  gettimeofday (&startwtime, NULL);
-  impBitonicSort();
-  gettimeofday (&endwtime, NULL);
+  int i;
+  for (i = 0; i < test_amount; i++) {
+    init();
 
-  seq_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
-		      + endwtime.tv_sec - startwtime.tv_sec);
+    // [Start Time]
+    gettimeofday (&startwtime, NULL);
+    impBitonicSort();
+    gettimeofday (&endwtime, NULL);
+    // [End Time]
 
-  printf("Imperative wall clock time = %f\n", seq_time);
+    seq_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
+            + endwtime.tv_sec - startwtime.tv_sec);
+    printf("[%d] Imperative wall clock time = %f\n", i, seq_time);
+    fprintf(log_file, "[%d] Imperative wall clock time = %f\n", i, seq_time);
+    
+    test();
+  }
 
-  test();
+  printf("-----------------------------------------\n");
+  fprintf(log_file, "-----------------------------------------\n");
+  for (i = 0; i < test_amount; i++) {
+    init();
 
-  init();
-  gettimeofday (&startwtime, NULL);
-  sort();
-  gettimeofday (&endwtime, NULL);
+    // [Start Time]
+    gettimeofday (&startwtime, NULL);
+    parBitonicSort();
+    gettimeofday (&endwtime, NULL);
+    // [End Time]
 
-  seq_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
-		      + endwtime.tv_sec - startwtime.tv_sec);
+    seq_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
+            + endwtime.tv_sec - startwtime.tv_sec);
+    printf("[%d] Parallel wall clock time = %f\n", i, seq_time);
+    fprintf(log_file, "[%d] Parallel wall clock time = %f\n", i, seq_time);
+    
+    test();
 
-  printf("Recursive wall clock time = %f\n", seq_time);
+    if (i < test_amount - 1) {
+      output_file = fopen("output/output.txt", "w");
+      if (output_file != NULL) {
+        int j;
+        for (j = 0; j < N; j++)
+          fprintf(output_file, "%d\n", arr[j]);
+        fclose(output_file);
+      }
+    }
+    
+  }
+  printf("-----------------------------------------\n");
+  fprintf(log_file, "-----------------------------------------\n\n\n");
+  fclose(log_file);
 
-  test();
-
-  // print();
+  return 0;
 }
 
 /** -------------- SUB-PROCEDURES  ----------------- **/ 
@@ -99,38 +156,41 @@ void test() {
   int pass = 1;
   int i;
   for (i = 1; i < N; i++) {
-    pass &= (a[i-1] <= a[i]);
+    pass &= (arr[i-1] <= arr[i]);
   }
 
-  printf(" TEST %s\n",(pass) ? "PASSed" : "FAILed");
+  printf("    TEST %s\n",(pass) ? "PASSED" : "FAILED");
+  fprintf(log_file, "    TEST %s\n",(pass) ? "PASSED" : "FAILED");
 }
 
 
-/** procedure init() : initialize array "a" with data **/
+/** procedure init() : initialize array "arr" with data **/
 void init() {
+  srand(0);
   int i;
   for (i = 0; i < N; i++) {
-    // a[i] = rand() % N; // (N - i);
-    a[i] = (N - i);
+    arr[i] = rand() % N; // (N - i);
+    // a[i] = (N - i);
   }
 }
 
-/** procedure  print() : print array elements **/
+
+/** procedure  print() : print arr array elements **/
 void print() {
   int i;
   for (i = 0; i < N; i++) {
-    printf("%d\n", a[i]);
+    printf("%d ", arr[i]);
   }
-  printf("\n");
 }
+
 
 
 /** INLINE procedure exchange() : pair swap **/
 inline void exchange(int i, int j) {
   int t;
-  t = a[i];
-  a[i] = a[j];
-  a[j] = t;
+  t = arr[i];
+  arr[i] = arr[j];
+  arr[j] = t;
 }
 
 
@@ -141,7 +201,7 @@ inline void exchange(int i, int j) {
    then a[i] and a[j] are interchanged.
 **/
 void compare(int i, int j, int dir) {
-  if (dir==(a[i]>a[j])) 
+  if (dir==(arr[i]>arr[j])) 
     exchange(i,j);
 }
 
@@ -196,20 +256,23 @@ void sort() {
   imperative version of bitonic sort
 */
 void impBitonicSort() {
-
   int i,j,k;
   
   for (k=2; k<=N; k=2*k) {
     for (j=k>>1; j>0; j=j>>1) {
       for (i=0; i<N; i++) {
-	int ij=i^j;
-	if ((ij)>i) {
-	  if ((i&k)==0 && a[i] > a[ij]) 
-	      exchange(i,ij);
-	  if ((i&k)!=0 && a[i] < a[ij])
-	      exchange(i,ij);
-	}
+        int ij=i^j;
+        if ((ij)>i) {
+          if ((i&k)==0 && arr[i] > arr[ij]) 
+            exchange(i,ij);
+          if ((i&k)!=0 && arr[i] < arr[ij])
+            exchange(i,ij);
+        }
       }
     }
   }
+}
+
+void parBitonicSort() {
+  
 }
